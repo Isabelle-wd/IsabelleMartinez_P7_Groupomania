ae8bdcea4cdec283a1125bfbba1e1f4db29bf3d3
isabelle-wd <isabelle.martinez.wd@gmail.com>
2021-08-03 14:53:12 +0200

    navbar

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import {Navbar, Nav, Container} from "react-bootstrap";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Post from "./pages/Post";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import { Layout } from "./components/Layout";
+import { NavigationBar } from "./components/NavigationBar";
 
 
 
@@ -15,24 +15,9 @@ function App() {
   
   return (
     <div className="App">
+      <NavigationBar/>
       <Layout>
-        <Router>
-          <Navbar sticky="top" bg="dark" variant="dark" expand="lg">
-            <Container>
-              <Navbar.Brand href="/">Groupomania</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <Nav.Link href="/">Publications</Nav.Link>
-                  <Nav.Link href="/CreatePost">Créer une publication</Nav.Link>
-                  <Nav.Link href="/login">Se connecter</Nav.Link>
-                  <Nav.Link href="/signup">S'inscrire</Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-          <br />
-    
+        <Router>   
             <Switch>
             <Route exact path="/" component={Home}/>
             <Route path="/CreatePost" component={CreatePost}/>
---
+++ Frontend/src/components/NavigationBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {Navbar, Nav} from "react-bootstrap";
+import styled from "styled-components";
+
+const Styles = styled.div`
+    .navbar {
+        background-color: #222;
+    }
+
+    .navbar-brand, .navbar-nav .nav-link {
+        color: #bbb;
+
+        &:hover {
+            color: white;
+        }
+    }
+`;
+
+export const NavigationBar = () => (
+    <Styles>
+        <Navbar expand="lg">
+              <Navbar.Brand className="ms-3" href="/">
+                <img
+                  alt=""
+                  src= "../images/icon.svg"
+                  width= "50"
+                  height= "50"
+                  className="d-inline-block align-top"
+        />{' '}
+                Groupomania</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end mx-3">
+                <Nav>
+                  <Nav.Link href="/CreatePost">Créer une publication</Nav.Link>
+                  <Nav.Link href="/login">Se connecter</Nav.Link>
+                  <Nav.Link href="/signup">S'inscrire</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+          </Navbar>
+    </Styles>
+)
