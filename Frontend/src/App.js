91e5d1b7f074c063bc558ab598019a453f1627b5
isabelle-wd <isabelle.martinez.wd@gmail.com>
2021-10-13 11:09:25 +0200

    navbar

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
-import {Navbar, Nav} from "react-bootstrap";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import axios from "axios";
 
-import Avatar from "@material-ui/core/Avatar";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Post from "./pages/Post";
@@ -13,75 +13,19 @@ import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import { NoMatch } from "./pages/NoMatch";
 
-import { AuthContext } from "./helpers/AuthContext";
+
 
 function App() { 
-  const [authState, setAuthState] = useState({
-    username:"", 
-    id: 0, 
-    status: false,
-  });
+ 
 
-  useEffect(() => {
-    axios.get(
-      "http://localhost:3001/auth/auth", {
-          headers: { Authorization: "Bearer " + localStorage.getItem("accessToken"),
-        },
-      })
-      .then((response) => {
-        if (response.data.error) {
-          setAuthState({ ...authState, status: false });
-        } else {
-          setAuthState({
-            username: response.data.username,
-            id: response.data.id,
-            status: true,
-          });
-        }
-      });
-      // eslint-disable-next-line
-  }, []);
-
-  const logout = () => {
-    localStorage.removeItem("accessToken");
-    setAuthState({ username: "", id: 0, status: false });
-  };
 
   
     return (
       <div className="App">
-        <AuthContext.Provider value={{ authState, setAuthState }}>          
+                 
          
             <Router>              
-              <Navbar expand="lg" bg="dark" variant="dark" fixed="top" className="mb-3">
-                    <Navbar.Brand className="ms-3" href="/">
-                      <img
-                        alt="logo groupomania"
-                        src= "/images/icon.svg"
-                        width= "50"
-                        height= "50"
-                        className="d-inline-block align-top"
-              />{" "}
-                      Groupomania</Navbar.Brand>
-                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                  <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end mx-3">
-                    <Nav>          
-                      {!authState.status ? (
-                      <>
-                      <Nav.Link href="/login">Connexion</Nav.Link>
-                      <Nav.Link href="/signup">Inscription</Nav.Link>
-                      </>
-                      ) : (     
-                      <>               
-                      <Avatar alt="photo utilisateur" src="images/profile_pic.png"></Avatar>
-                      <Nav.Link href="/Profile/:id" className="text-white ms-2">{authState.username}</Nav.Link>
-                      <Nav.Link href="/CreatePost">Créer une publication</Nav.Link>    
-                      </>                      
-                      )}                     
-                      {authState.status && <button className ="btn btn-link mb-2" onClick={logout}> Déconnexion </button>} 
-                    </Nav>
-                  </Navbar.Collapse>
-              </Navbar>
+            <Navbar />
    
               <Switch>
                 <Route path="/" exact component={Home}/>
@@ -93,7 +37,7 @@ function App() {
                 <Route path="*" exact component={NoMatch}/>
               </Switch> 
             </Router>          
-        </AuthContext.Provider>    
+           
       </div>
     );
   }
---
+++ Frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from "react";
+import './Navbar.css';
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import axios from "axios";
+import Avatar from "@material-ui/core/Avatar";
+//import PostAddIcon from '@mui/icons-material/PostAdd';
+
+import { AuthContext } from "../../helpers/AuthContext";
+
+function Navbar() { 
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+  const [authState, setAuthState] = useState({
+    username:"", 
+    id: 0, 
+    status: false,
+  });
+
+  useEffect(() => {
+    axios.get(
+      "http://localhost:3001/auth/auth", {
+          headers: { Authorization: "Bearer " + localStorage.getItem("accessToken"),
+        },
+      })
+      .then((response) => {
+        if (response.data.error) {
+          setAuthState({ ...authState, status: false });
+        } else {
+          setAuthState({
+            username: response.data.username,
+            id: response.data.id,
+            status: true,
+          });
+        }
+      });
+      // eslint-disable-next-line
+  }, []);
+
+  const logout = () => {
+    localStorage.removeItem("accessToken");
+    setAuthState({ username: "", id: 0, status: false });
+  };
+
+  
+    return (
+      <div className="App">
+        <AuthContext.Provider value={{ authState, setAuthState }}>          
+         
+            <Router>              
+               <nav className="navbar">
+                <div className="navbar-container">
+                    <img
+                        alt="logo"
+                        src= "images/icon-left-font-monochrome-white.svg"                       
+                        width="350"
+                        className="icon-logo"
+                    />{" "}
+                     <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? "fas fa-times" : "fas fa-bars"} />
+                    </div>   
+                  <ul className="nav-menu">         
+                      {!authState.status ? (
+                      <>
+                      <li className="nav-item">
+                            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                                Accueil
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/login" className="nav-links" onClick={closeMobileMenu}>
+                                Connexion
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/signup" className="nav-links" onClick={closeMobileMenu}>
+                                Inscription
+                            </Link>
+                        </li>
+                      </>
+                      ) : (     
+                      <>               
+                      <li className="nav-item">
+                            <Link to="/profile/:id" className="nav-links" onClick={closeMobileMenu}>
+                                <Avatar alt="photo utilisateur" src="images/profile_pic.png">{authState.username}</Avatar>
+                            </Link>
+                        </li>
+    			<li className="nav-item">
+                            <Link to="/createPost" className="nav-links" onClick={closeMobileMenu}>
+                            	<span class="material-icons-outlined">
+					
+			    	</span>
+                            </Link>
+                        </li>   
+                      </>                      
+                      )}                     
+                      {authState.status && 
+			<li className="nav-item" onClick={logout}>
+			   <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                                Deconnexion
+                            </Link>
+			</li>} 
+            </ul>
+            </div>
+                    </nav>
+                               
+            </Router>          
+        </AuthContext.Provider>    
+      </div>
+    );
+  }
+
+export default Navbar;
